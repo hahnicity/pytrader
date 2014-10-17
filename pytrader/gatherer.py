fcd3f1adfea0616d26d4bfcc8645de26de382f58
hahnicity <grehm87@gmail.com>
2014-10-17 01:55:27 -0700

    add funky little algorithm for merging eps data frames with prices

--- pytrader/gatherer.py
+++ pytrader/gatherer.py
@@ -1,4 +1,6 @@
 from cowboycushion.multiprocessing_limiter import RedisMultiprocessingLimiter
+from numpy import append, array
+from pandas import DataFrame
 
 
 def gather_data(args, DataImpl):
@@ -21,7 +23,7 @@ def gather_data(args, DataImpl):
     data_client.join()
     dfs = [job.get() for job in jobs]
     # just keep it like this until we need otherwise
-    merged = merge_data_frames(*dfs)
+    return merge_data_frames(*dfs)
 
 
 def merge_unequal_data_frames(dfa, dfb):
@@ -29,15 +31,37 @@ def merge_unequal_data_frames(dfa, dfb):
     Given the assumptions of our system, that all dates will revolve around price data,
     we want to take unequal data frames like eps and overlay them on the price data
     frame.
+
+    We do not have the prettiest algorithm for doing this but it works
     """
-    dfa_idx = dfa.index.values
-    dfb_idx = dfb.index.values
+    def find_closest_idx(greater_idx, lesser_idx, idx):
+        val = greater_idx[idx]
+        for i, lesser_val in enumerate(lesser_idx):
+            if val == lesser_val:
+                return i
+            elif i > 0 and lesser_idx[i - 1] < val and lesser_val > val:
+                return i - 1
+            elif i == len(lesser_idx) - 1:
+                return i
+        else:
+            import pdb; pdb.set_trace()
+            # Corner case unexpected
+
     if len(dfa) > len(dfb):
-        pass
+        greater = dfa
+        lesser = dfb
     else:
-        pass
-    # For the moment string comparisons will do just fine
-    import pdb; pdb.set_trace()
+        greater = dfb
+        lesser = dfa
+
+    mapped_array = array([])
+    for i, k in enumerate(greater.values):
+        closest_idx = find_closest_idx(greater.index.values, lesser.index.values, i)
+        mapped_array = append(mapped_array, lesser.values[closest_idx])
+
+    return greater.combine_first(
+        DataFrame(mapped_array, index=greater.index.values, columns=[lesser.columns[0]])
+    )
 
 
 def merge_data_frames(dfa, dfb):
